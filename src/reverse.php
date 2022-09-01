d3ae8fc44009d765c3ece42db0415e325d38577f
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-09-01 14:14:56 +0200

    Create reverse.php

---
+++ src/reverse.php
@@ -0,0 +1,44 @@
+<?php
+
+$config = require $projectDir . 'config.php';
+
+try {
+    $connection = new PDO(
+        'mysql:host=' . $config['host'] . ';dbname=' . $config['database'] . ';charset=utf8',
+        $config['username'],
+        $config['password'],
+        [
+            PDO::ATTR_TIMEOUT => 130,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    );
+} catch (PDOException $e) {
+    internalServerErrorResponse();
+}
+
+try {
+    $statement = $connection->prepare('SELECT youtube_id FROM channel WHERE twitter_handle = :twitter_handle;');
+    $statement->execute(['twitter_handle' => $twitterHandle]);
+} catch (PDOException $e) {
+    $connection = null;
+    internalServerErrorResponse();
+}
+
+$fetchedChannels = $statement->fetchAll();
+
+if (! $fetchedChannels) {
+  notFoundErrorResponse();
+}
+
+$channelId = $fetchedChannels[0]['youtube_id'];
+
+if (! $channelId) {
+  internalServerErrorResponse();
+  exit;
+}
+
+http_response_code(200);
+
+echo json_encode(['channelId' => $channelId]);
+
+$connection = null;
